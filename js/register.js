7dd372d0d368931bb541c607af6077e28bd7f358
TranGiaHy <trangiahy31082004@gmail.com>
2026-06-27 15:14:03 +0700

    Update Register

--- js/register.js
+++ js/register.js
@@ -1,6 +1,16 @@
-function togglePassword(id) {
-  let input = document.getElementById(id);
-  input.type = (input.type === "password") ? "text" : "password";
+function togglePassword(inputId, iconId) {
+  let input = document.getElementById(inputId);
+  let icon = document.getElementById(iconId);
+
+  if (input.type === "password") {
+    input.type = "text";
+    icon.classList.remove("fa-eye-slash");
+    icon.classList.add("fa-eye");
+  } else {
+    input.type = "password";
+    icon.classList.remove("fa-eye");
+    icon.classList.add("fa-eye-slash");
+  }
 }
 
 function register() {
